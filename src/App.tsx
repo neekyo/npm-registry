ec904bb5f81ffab7de9279cb5519cd5343620bb1
neekyo <codebyns@gmail.com>
2024-06-26 16:27:24 -0400

    added api handling, header css, search input function, data fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
-import { createBrowserRouter, RouterProvider  } from "react-router-dom";
-import Root from './pages/Root';
-import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
-import DetailsPage from './pages/DetailsPage';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
+import DetailsPage from "./pages/DetailsPage";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Root />,
     children: [
       {
         index: true,
-        element: <HomePage />
+        element: <HomePage />,
       },
       {
-        path: '/search',
-        element: <SearchPage />
+        path: "/search",
+        element: <SearchPage />,
+        loader: async ({ request }) => {
+          const { searchParams } = new URL(request.url);
+          const term = searchParams.get('term');
+
+          // if (!term) {
+          //   throw new Error('Search term must be provided');
+          // }
+
+          const res = await fetch(
+            `https://registry.npmjs.org/-/v1/search?text=${term}`
+          );
+          const data = await res.json();
+
+          return data.objects;
+        },
       },
       {
-      path: '/packages/:name',
-      element: <DetailsPage />
-      }
-    ]
-  }
-])
+        path: "/packages/:name",
+        element: <DetailsPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return <RouterProvider router={router} />;
-};
+}
 
-export default App
+export default App;
